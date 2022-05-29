59877967dfaab1f825a7bde8fbc3e655044dc29f
Code Sandbox <hello@codesandbox.io>
2022-05-29 12:23:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ebbdc71a0b7e0a6c131f522762f61c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d236d49a40f1aabd619f61f654e39f7",
+    "url": "https://bgoonz.github.io/csb-fz0em4/index.html"
+  },
+  {
+    "revision": "14ebe2824a59dee9ec7f",
+    "url": "https://bgoonz.github.io/csb-fz0em4/static/css/2.fd8d6946.chunk.css"
+  },
+  {
+    "revision": "66c0ca145c9aff28b45d",
+    "url": "https://bgoonz.github.io/csb-fz0em4/static/css/main.3171e0cf.chunk.css"
+  },
+  {
+    "revision": "14ebe2824a59dee9ec7f",
+    "url": "https://bgoonz.github.io/csb-fz0em4/static/js/2.03525354.chunk.js"
+  },
+  {
+    "revision": "66c0ca145c9aff28b45d",
+    "url": "https://bgoonz.github.io/csb-fz0em4/static/js/main.336b441e.chunk.js"
+  },
+  {
+    "revision": "07b1fe1d9a03ec5b2bc4",
+    "url": "https://bgoonz.github.io/csb-fz0em4/static/js/runtime~main.fdf90604.js"
+  }
+]);
